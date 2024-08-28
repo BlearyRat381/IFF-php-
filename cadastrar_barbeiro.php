f27fb199a7b8d94100dfdb57ebef777a6bda726d
BlearyRat381 <37387115+BlearyRat381@users.noreply.github.com>
2024-08-28 17:44:36 -0300

    Add files via upload

---
+++ cadastrar_barbeiro.php
@@ -0,0 +1,18 @@
+<?php
+
+include "conexao.php";
+
+
+echo "<pre>";
+print_r($_POST);
+
+$nome_usuario = $_POST['nome_usuario'];
+$nome_usuario = $_POST['nome_usuario'];
+$nome_usuario = $_POST['nome_usuario'];
+$nome_usuario = $_POST['nome_usuario'];
+$nome_usuario = $_POST['nome_usuario'];
+$nome_usuario = $_POST['nome_usuario'];
+$nome_usuario = $_POST['nome_usuario'];
+
+$sql =
+?>
