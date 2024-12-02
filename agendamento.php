e6669352ae91187a1acad56760aaa264ad9d85b6
BlearyRat381 <37387115+BlearyRat381@users.noreply.github.com>
2024-12-02 17:43:53 -0300

    Atualização agendamento

--- agendamento.php
+++ agendamento.php
@@ -1,98 +1,71 @@
 <?php
 include "connection.php";
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-Br">
+
 <head>
-  <title>Agendar Atendimento</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Tela de Agendamento</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
 
-<div class="container mt-3">
-  <h2>Agendamento</h2>
-  <form action="#.php" name="data">
-  <div class="mb-3 mt-3">
-      <label class="form-check-label">Cliente:</label>
-      <select class="form-select">
-        <option value="">Selecione um Cliente</option>
-        <?php
-        $sql = 'select * from usuarios where id_grupo = 3';
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-          while($row = $result->fetch_assoc()) {
-            echo "<option value=".$row['id_usuario'].">".$row['nome_usuario']."</option>";
-          }
-        }
-        ?>
-      </select> 
-      <a href="paginadecadastro.php" class="btn btn-success" style="float: right;">+</a>
+    <div class="container mt-3">
+        <h2>Criar agendamento</h2>
+        <form action="criar_agendamento.php" method="post">
+
+            <div class="mb-3">
+                <label for="data">Data do Agendamento:</label>
+                <input type="date" class="form-control" name="data" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="id_usuario">Cliente:</label>
+                <select class="form-select" name="id_usuario" required>
+                    <option>Selecione um Cliente</option>
+                    <?php
+                    // Consulta de clientes
+                    $sql = 'SELECT * FROM usuario WHERE id_grupo = 2'; // 2 é o id_grupo para Cliente
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()) {
+                            echo "<option value='".$row['id_usuario']."'>".$row['nome']."</option>";
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="servico">Escolha o Serviço:</label>
+                <select class="form-select" name="servico" id="servico" required>
+                    <?php
+                    // Consulta de serviços
+                    $sql = 'SELECT * FROM servicos';
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<option value='" . $row['id_servico'] . "'>" . $row['nome'] . "</option>";
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
+
+            <div class="botoes">
+                <a href="index.php" class="btn btn-success">Novo Cliente</a>
+                <a href="index.php" class="btn btn-primary">Voltar</a>
+                <button type="submit" class="btn btn-primary">Confirmar</button>
+            </div>
+
+        </form>
     </div>
-    <div class="mb-3 mt-3">
-      <label class="form-check-label">Serviço:</label>
-      <select class="form-select">
-        <option value="">Selecione um Serviço</option>
-        <?php
-        $sql = 'select * from servicos';
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-          while($row = $result->fetch_assoc()) {
-            echo "<option>".$row['servico']."</option>";
-          }
-        }
-        ?>
-      </select>
-    </div>
-    <div class="mb-3 mt-3">
-      <label class="form-check-label">Barbeiro:</label>
-      <select class="form-select">
-        <option value="">Selecione um Barbeiro</option>
-        <?php
-        $sql = 'select * from usuarios where id_grupo = 2';
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-          while($row = $result->fetch_assoc()) {
-            echo "<option value=".$row['id_usuario'].">".$row['nome_usuario']."</option>";
-          }
-        }
-        ?>
-      </select>
-    </div>
-    <div class="mb-3 mt-3">
-      <label class="form-check-label">Dia:</label>
-      <select class="form-select">
-        <option value="">Selecione um Dia</option>
-        <option value="0">Domingo</option>
-        <option value="1">Segunda-Feira</option>
-        <option value="2">Terça-Feira</option>
-        <option value="3">Quarta-Feira</option>
-        <option value="4">Quinta-Feira</option>
-        <option value="5">Sexta-Feira</option>
-        <option value="6">Sábado</option>
-      </select>
-    </div>
-    <div class="mb-3 mt-3">
-    <label class="form-check-label">Horário:</label>
-    <select class="form-select">
-    <option value="">Selecione um Horário</option>
-    <?php
-    $sql = 'select * from agenda';
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0){
-      while($row = $result->fetch_assoc()){
-        echo "<option>".$row['horario']."</option>";
-      }
-    }
-    ?>
-    </select>
-</div>
-    <button type="submit" class="btn btn-primary">Agendar</button>
-    <a href="index.php" class="btn btn-primary" style="float: right;">Voltar</a>
-  </form>
-</div>
 
 </body>
+
 </html>
